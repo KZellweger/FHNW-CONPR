41603af0a18d04200123d8ad35b1c4eda528a55a
Kevin Zellweger <kevin.zellweger@students.fhnw.ch>
2022-03-13 14:34:36 +0100

    semaphore simple

--- 03_Condition_Synchronization/src/main/java/as/semaphore/SemaphoreImpl.java
+++ 03_Condition_Synchronization/src/main/java/as/semaphore/SemaphoreImpl.java
@@ -2,6 +2,7 @@ package as.semaphore;
 
 public final class SemaphoreImpl implements Semaphore {
     private int value;
+    private Object lock = new Object();
 
     public SemaphoreImpl(int initial) {
         if (initial < 0) throw new IllegalArgumentException();
@@ -11,15 +12,29 @@ public final class SemaphoreImpl implements Semaphore {
     @Override
     public int available() {
         return value;
+//        synchronized (lock) {
+//        }
     }
 
     @Override
     public void acquire() {
-
+        synchronized (lock) {
+            while (available() == 0) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                }
+            }
+            value--;
+            lock.notifyAll();
+        }
     }
 
     @Override
     public void release() {
-
+        synchronized (lock) {
+            value++;
+            lock.notifyAll();
+        }
     }
 }
--- 03_Condition_Synchronization/src/main/java/worksheet/Semaphore.java
+++ 03_Condition_Synchronization/src/main/java/worksheet/Semaphore.java
@@ -2,6 +2,7 @@ package worksheet;
 
 public final class Semaphore {
     private int value;
+    private Object lock = new Object();
 
     public Semaphore(int initial) {
         if (initial < 0) throw new IllegalArgumentException();
@@ -10,30 +11,28 @@ public final class Semaphore {
 
     public int available() {
         return value;
+//        synchronized (lock) {
+//        }
     }
 
     public void acquire() {
-
-
-
-
-
-
-
-
-
+        synchronized (lock) {
+            while (available() == 0) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                }
+            }
+            value--;
+            lock.notifyAll();
+        }
     }
 
     public void release() {
-
-
-
-
-
-
-
-
-
+        synchronized (lock) {
+            value++;
+            lock.notifyAll();
+        }
     }
 
 }
