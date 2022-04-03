bb8de34b666ab2793d8220c3a5fea65608f26ae5
Kevin Zellweger <kevin.zellweger@students.fhnw.ch>
2022-04-03 08:44:07 +0200

    fideling during lecture

--- 06_Lock_Free/src/main/java/atomics/ConcurrentVariable.java
+++ 06_Lock_Free/src/main/java/atomics/ConcurrentVariable.java
@@ -1,5 +1,6 @@
 package atomics;
 
+import java.util.Locale;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 
@@ -11,6 +12,49 @@ public interface ConcurrentVariable<A> {
     void update(Function<A, A> f);
 }
 
+
+final class Testi {
+    public static void main(String[] args) {
+        //atomic();
+        unsafe();
+    }
+
+    private static void atomic() {
+        for (int i = 0; i < 100; i++) {
+            ConcurrentVariable<String> var1 = new AtomicVariable<>();
+            var1.set("hello");
+            Thread t1 = new Thread(() -> {
+                var1.update(s -> s.toUpperCase(Locale.ROOT));
+            });
+            Thread t2 = new Thread(() -> {
+                var1.update(s -> s.repeat(42));
+            });
+
+            t1.start();
+            t2.start();
+
+            new Thread(() -> System.out.println(var1.get())).start();
+        }
+    }
+
+    private static void unsafe() {
+        for (int i = 0; i < 100; i++) {
+            ConcurrentVariable<String> var1 = new UnsafeVariable<>();
+            var1.set("hello");
+            Thread t1 = new Thread(() -> {
+                var1.update(s -> s.toUpperCase(Locale.ROOT));
+            });
+            Thread t2 = new Thread(() -> {
+                var1.update(s -> s.repeat(42));
+            });
+
+            t1.start();
+            t2.start();
+
+            new Thread(() -> System.out.println(var1.get())).start();
+        }
+    }
+}
 
 final class AtomicVariable<A> implements ConcurrentVariable<A> {
     private final AtomicReference<A> reference = new AtomicReference<>();
@@ -34,6 +78,25 @@ final class AtomicVariable<A> implements ConcurrentVariable<A> {
                 break;
             }
         }
+    }
+}
+
+final class UnsafeVariable<A> implements ConcurrentVariable<A> {
+    private A reference;
+
+    @Override
+    public A get() {
+        return reference;
+    }
+
+    @Override
+    public void set(A a) {
+        reference = a;
+    }
+
+    @Override
+    public void update(Function<A, A> f) {
+        reference = f.apply(reference);
     }
 }
 
